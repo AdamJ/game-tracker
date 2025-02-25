489b3659f7e935530016c048b43b0063d13e6640
AdamJ <AdamJ@users.noreply.github.com>
2025-02-25 22:31:58 +0000

    Update Deploy Action 6d2716376e45e293a2d89cd293d4b5e22f5c3371

--- src/components/mana-icon.ts
+++
@@ -1,9 +0,0 @@
-import { registerIconLibrary } from '@shoelace-style/shoelace/dist/utilities/icon-library.js';
-
-registerIconLibrary('ms', {
-  resolver: name => {
-    const filename = name.replace(/^ms-/, '');
-    return `https://cdn.jsdelivr.net/npm/mana-font@latest/svg/${filename}.svg`;
-  },
-  mutator: svg => svg.setAttribute('fill', 'currentColor')
-});
