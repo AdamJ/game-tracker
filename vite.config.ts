aa5f1ab497b8c1113835a48f7e493f0cf67e8c97
AdamJ <adam.jolicoeur@gmail.com>
2025-02-25 20:20:45 -0500

    Add 404, update routing, update vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path'
+import { fileURLToPath } from 'url'
 import { VitePWA } from 'vite-plugin-pwa';
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = resolve()
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/task-it",
+  base: "/task-it/",
   build: {
     sourcemap: true,
     assetsDir: "code",
     target: ["esnext"],
+    outDir: "dist/",
     cssMinify: true,
     lib: false
   },
