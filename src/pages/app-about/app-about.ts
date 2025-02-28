9fddbbe50fa85fddef345f3b52d9262c0813969f
AdamJ <adam.jolicoeur@gmail.com>
2025-02-27 21:23:44 -0500

    Fix build errors

--- src/pages/app-about/app-about.ts
+++ src/pages/app-about/app-about.ts
@@ -1,9 +1,8 @@
 import { LitElement, html } from 'lit';
 import { customElement } from 'lit/decorators.js';
-import { resolveRouterPath } from '../../router';
 
 // You can also import styles from another file
-// if you prefer to keep your CSS seperate from your component
+// if you prefer to keep your CSS separate from your component
 import { styles } from './about-styles';
 
 import { styles as sharedStyles } from '../../styles/shared-styles'
